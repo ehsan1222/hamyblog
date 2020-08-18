82c3b12b8e37f95fbdc621f37e9345ec74c4a238
ehsan1222 <s.e.maddahi.ir@gmail.com>
2020-08-18 15:23:45 +0430

    init commit

---
+++ src/main/java/ir/hamyblog/HamyblogApplication.java
@@ -0,0 +1,13 @@
+package ir.hamyblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HamyblogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HamyblogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ir/hamyblog/HamyblogApplicationTests.java
@@ -0,0 +1,13 @@
+package ir.hamyblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HamyblogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
