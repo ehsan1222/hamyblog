551e29c45dceac75b44aa580444dfc38b4ddd121
ehsan1222 <s.e.maddahi.ir@gmail.com>
2020-08-28 20:57:09 +0430

    implement comment repoitory and unit testing

---
+++ src/main/java/ir/hamyblog/repositories/CommentRepository.java
@@ -0,0 +1,10 @@
+package ir.hamyblog.repositories;
+
+import ir.hamyblog.entities.Comment;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface CommentRepository extends JpaRepository<Comment, UUID> {
+
+}
---
+++ src/test/java/ir/hamyblog/repositories/CommentRepositoryTest.java
@@ -0,0 +1,63 @@
+package ir.hamyblog.repositories;
+
+import ir.hamyblog.entities.Article;
+import ir.hamyblog.entities.Comment;
+import ir.hamyblog.entities.User;
+import org.assertj.core.api.Assertions;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.Optional;
+import java.util.UUID;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RunWith(SpringRunner.class)
+@DataJpaTest
+@EnableJpaAuditing
+public class CommentRepositoryTest {
+
+    @Autowired
+    private TestEntityManager manager;
+
+    @Autowired
+    private CommentRepository commentRepository;
+
+    @Test
+    public void shouldRemoveCommentAfterRemoveArticle(){
+        Article article = new Article("titie", UUID.randomUUID(), "content");
+        User user = new User("usernmae", "password", "ehsan maddahi");
+        manager.persist(article);
+        manager.persist(user);
+
+        Comment comment = new Comment("content comment", user, article);
+        article.newComment(comment);
+        manager.persist(article);
+
+        manager.remove(article);
+
+        assertThat(commentRepository.findAll()).isEmpty();
+    }
+
+    @Test
+    public void shouldChangeLastModifiedDateAfterCommentChanged() {
+        Article article = new Article("titie", UUID.randomUUID(), "content");
+        User user = new User("usernmae", "password", "ehsan maddahi");
+        manager.persist(article);
+        manager.persist(user);
+
+        Comment comment = new Comment("content comment", user, article);
+        article.newComment(comment);
+        manager.persist(article);
+
+        final Optional<Comment> commentOptional = commentRepository.findById(comment.getUuid());
+        System.out.println(commentOptional.get().getLastModified());
+    }
+
+
+}
