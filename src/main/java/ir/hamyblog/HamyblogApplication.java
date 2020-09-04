ff6db1047175bcafb75d7b49e532641a9b09a5be
ehsan1222 <s.e.maddahi.ir@gmail.com>
2020-09-04 19:54:21 +0430

    add user defaults

--- src/main/java/ir/hamyblog/HamyblogApplication.java
+++ src/main/java/ir/hamyblog/HamyblogApplication.java
@@ -1,13 +1,32 @@
 package ir.hamyblog;
 
+import ir.hamyblog.entities.User;
+import ir.hamyblog.model.Role;
+import ir.hamyblog.model.UserRegisterIn;
+import ir.hamyblog.services.UserService;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class HamyblogApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HamyblogApplication.class, args);
+	}
+
+	@Autowired
+	private UserService userService;
+
+	@Bean
+	InitializingBean initializingBean() {
+		return () -> {
+			userService.addUser(new UserRegisterIn("ehsan", "password", "ehsan maddahi"));
+			userService.addUser(new UserRegisterIn("omid", "passwd", "omid mahmodi"));
+			userService.updateRole("omid", Role.ADMIN);
+		};
 	}
 
 }
