7fc3a6caa0eee6a2d5e2f5d0142f81a2b373b00a
Ehsan Madahi <e.madahi@vas24.ir>
2020-09-04 19:41:12 +0430

    some changes in user entity

--- src/main/java/ir/hamyblog/entities/User.java
+++ src/main/java/ir/hamyblog/entities/User.java
@@ -1,18 +1,19 @@
 package ir.hamyblog.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import ir.hamyblog.model.Role;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.util.UUID;
 
 @Entity
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @NoArgsConstructor
 @Getter
+@Table(name = "\"User\"")
 public class User {
 
     @Id
@@ -21,9 +22,16 @@ public class User {
 
     @Column(unique = true)
     private String username;
+
+    @JsonIgnore
     private String password;
     private String fullName;
+
+    @JsonIgnore
     private String secretKey;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
 
     public User(String username, String password, String fullName, String secretKey) {
         uuid = UUID.randomUUID();
@@ -31,6 +39,7 @@ public class User {
         this.password = password;
         this.fullName = fullName;
         this.secretKey = secretKey;
+        this.role = Role.USER;
     }
 
     public void setPassword(String password) {
@@ -39,5 +48,9 @@ public class User {
 
     public void setFullName(String fullName) {
         this.fullName = fullName;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
     }
 }
