9750d9cc8b474ea1a4b60b6ce10dd5a98abf03eb
ehsan1222 <s.e.maddahi.ir@gmail.com>
2020-09-05 21:02:30 +0430

    implement illegalArgumentException handler

---
+++ src/main/java/ir/hamyblog/exceptions/GlobalExceptionHandler.java
@@ -0,0 +1,16 @@
+package ir.hamyblog.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<?> handleIllegalArgumentException(IllegalArgumentException e) {
+        return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+    }
+
+}
