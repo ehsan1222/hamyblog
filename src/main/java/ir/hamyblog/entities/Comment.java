b4d4d7c4abf61462cbe53ce076c928f68c52da1c
ehsan1222 <s.e.maddahi.ir@gmail.com>
2020-09-08 23:49:31 +0430

    add some ignore annotations and fix creation date problem in comment entity

--- src/main/java/ir/hamyblog/entities/Comment.java
+++ src/main/java/ir/hamyblog/entities/Comment.java
@@ -1,14 +1,15 @@
 package ir.hamyblog.entities;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.ManyToOne;
+import javax.persistence.*;
 import java.util.Date;
 import java.util.UUID;
 
@@ -25,18 +26,22 @@ public class Comment {
     private String content;
 
     @ManyToOne
+    @JsonIgnoreProperties({"uuid", "username", "role"})
     private User user;
 
     @ManyToOne
+    @JoinColumn
+    @JsonIgnore
     private Article article;
 
-    @CreatedDate
-    private Date lastModified;
+    @JsonFormat(timezone = "Asia/Tehran", pattern = "yyyy-MM-dd HH:mm")
+    private Date creationDate;
 
     public Comment(String content, User user, Article article) {
         this.uuid = UUID.randomUUID();
         this.content = content;
         this.user = user;
         this.article = article;
+        this.creationDate = new Date();
     }
 }
