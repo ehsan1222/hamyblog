4761ba908de1ed4b6bd97f45532e51974148ac17
ehsan1222 <s.e.maddahi.ir@gmail.com>
2020-09-09 20:43:40 +0430

    implements query dsl of retrieve all sorted comments of the article

--- src/main/java/ir/hamyblog/repositories/CommentRepository.java
+++ src/main/java/ir/hamyblog/repositories/CommentRepository.java
@@ -1,10 +1,12 @@
 package ir.hamyblog.repositories;
 
+import ir.hamyblog.entities.Article;
 import ir.hamyblog.entities.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface CommentRepository extends JpaRepository<Comment, UUID> {
-
+    List<Comment> findByArticleOrderByCreationDateDesc(Article article);
 }
